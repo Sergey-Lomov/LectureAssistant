330fec7dd8494c75d3af825c73f25bf2473fbaf7
serhii.lomov <serhii.lomov@itomy.ch>
2022-09-22 03:04:54 +0300

    Initial Commit

---
+++ LectureAssistant/LectureAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LectureAssistantApp.swift
+//  LectureAssistant
+//
+//  Created by serhii.lomov on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct LectureAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
